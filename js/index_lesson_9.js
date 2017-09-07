e2149bcb7b29057339f36f926847cd555964fca8
kamdemkogaing <31521726+kamdemkogaing@users.noreply.github.com>
2017-09-07 13:12:46 +0200

    js courses updated version 1

---
+++ js/index_lesson_9.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+	var HTMLprojectStart = '<div class="project-entry"></div>';
+	var HTMLprojectDates = '<div class="date-text">%data%</div>';
+	var HTMLprojectDescription = '<p><br>%data%</p>';
+	var HTMLprojectImage = '<img src="%data%">';
+
+  //projects Object declared
+	var projects = {
+    "projects" : [
+	    {
+	    	"title": "Course Developer",
+	    	"dates": "Feb 2014 - Current",
+	    	"description": "MAN IN BLACK",
+	    	"images" : ["img/kamdem.png","img/logo_kamdem.png","img/luca_elyas.jpg"],
+	    	"display": function firtProject() { return 'My first project function';}
+	    },
+	    {
+	    	"title": "Software Engineer",
+	    	"dates": "May 2013 - Jan 2014",
+	    	"description": "MAN IN BLACK",
+	    	"images" : ["img/kamdem.png","img/logo_kamdem.png","img/luca_elyas.jpg"],
+	    	"display": function secondProject() { return 'My second project function';}
+	    },
+	    {
+	    	"title": "Science Teacher",
+	    	"dates": "Jul 2012 - May 2013",
+	    	"description": "MAN IN BLACK",
+	    	"images" : ["img/kamdem.png","img/logo_kamdem.png","img/luca_elyas.jpg"],
+	    	"display": function thirdProject() { return 'My third project function';}
+	    }
+    ]
+	};
+
+	//Encapsulating Functions.
+	projects.display = function () {
+		for (project in projects.projects) {
+			$('#projects').append(HTMLprojectStart);
+
+			var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+			$(".project-entry:last").append(formattedTitle);
+
+			var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+			$(".project-entry:last").append(formattedTitle);
+
+			var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+			$(".project-entry:last").append(formattedDescription);
+
+			if (projects.projects[project].images.length > 0) {
+				for (image in projects.projects[project].images) {
+    			var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+    			$(".project-entry:last").append(formattedImage);
+				}
+			}
+		}
+	}
+
+});
