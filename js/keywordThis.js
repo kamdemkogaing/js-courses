408ed76a7e4f80f875ae97bc9ff9fcdc36f0e01a
kamdemkogaing <31521726+kamdemkogaing@users.noreply.github.com>
2017-09-18 15:48:06 +0200

    some changes

--- js/keywordThis.js
+++ js/keywordThis.js
@@ -1,14 +1,24 @@
 $(document).ready(function () {
 
     var fn = function (one, two) {
-        console.log(this, one, two);
+      console.log(this, one, two);
     };
 
-    var r = {}, g = {}, b = {}, y = {};
+    var r = {red: "red"}, g = {green: "green"}, b = {blue: "blue"}, y = {yellow: "yellow"};
+    r.method = fn; // r = {red: "red",function (one, two) { console.log(this, one, two)};
 
-    r.method = fn;
+    r.method(g,b); // r,g,b
 
-    r.method.call(y, g, b);
+    fn(g,b);  // global Object,g,b
 
-    console.log(r);
+    //La méthode call() réalise un appel à une fonction avec une valeur this et les arguments fournis individuellement.
+    fn.call(r,g,b); //r,g,b
+
+    r.method.call(y,g,b); //y,g,b
+    
+    //Exécute un morceau de code ou une fonction après un délai determiné.
+    // setTimeout(fn, 1000); //global Object, undefined, undefined
+
+    setTimeout(r.method, 1000); //global Object, undefined, undefined
+
 });
