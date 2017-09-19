2cdc79a495bc98f141cf9aa4a5db6d333c172bc4
kamdemkogaing <31521726+kamdemkogaing@users.noreply.github.com>
2017-09-19 08:13:07 +0200

    some changes

--- js/theFunctions.js
+++ js/theFunctions.js
@@ -1,3 +1,27 @@
 $(document).ready(function () {
-    
+    var gold = {a:1};
+    console.log(gold.a); //1
+    console.log(gold.z); //undefined
+
+    //merge object gold into {}
+    //blue = {a:1};
+    var blue = extend({}, gold);
+
+    blue.b = 2; // blue = {a:1,b:2}
+
+    console.log(blue.a); //1
+    console.log(blue.b); //2
+    console.log(blue.z); //undefined
+
+    //new prototype rose from Object gold created
+    var rose = Object.create(gold);
+    rose.b = 2;
+
+    console.log(rose.a); //1
+    console.log(rose.b); //2
+    console.log(rose.z); //undefined
+
+    gold.z = 2;
+    console.log(console.z); //
+ 
 });
