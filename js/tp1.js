b16e952f528f7c79ff511ae66eb5798b6697f4d6
kamdemkogaing <31521726+kamdemkogaing@users.noreply.github.com>
2017-10-13 08:30:30 +0200

    somes commit to 13_10_17

--- js/tp1.js
+++ js/tp1.js
@@ -14,37 +14,59 @@ $(document).ready(function () {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++
 
-   //Select all element with Class .spoiler in to an array
-	 var elements = document.querySelectorAll('.spoiler');
+  //  //Select all element with Class .spoiler in to an array
+	 // var elements = document.querySelectorAll('.spoiler');
 
-   //element = some className like  ".spoiler"
-	 var createSpoilerButton = function (element) {
+  //  //element = some className like  ".spoiler"
+	 // var createSpoilerButton = function (element) {
 
-     //Button Element created
-	 	 var button = document.createElement('button');
-	 	 button.innerHTML = 'Afficher le spoiler';
+  //    //Button Element created
+	 // 	 var button = document.createElement('button');
+	 // 	 button.innerHTML = 'Afficher le spoiler';
 
-     //Span Element created
-	 	 var span = document.createElement('span');
-	 	 span.className = 'spoiler-content';
-	 	 span.innerHTML = element.innerHTML;
+  //    //Span Element created
+	 // 	 var span = document.createElement('span');
+	 // 	 span.className = 'spoiler-content';
+	 // 	 span.innerHTML = element.innerHTML;
 
-      //Elements added to DOM
-      element.innerHTML = '';
-	 	  element.appendChild(button);
-	 	  element.appendChild(span);
+  //     //Elements added to DOM
+  //     element.innerHTML = '';
+	 // 	  element.appendChild(button);
+	 // 	  element.appendChild(span);
 
-	 	  //Click Event added to Button Element
-	 	  button.addEventListener('click', function () {
-	 	  	button.parentNode.removeChild(button);
-	 	  	span.classList.add('visible');
-	 	  });
-	 };
+	 // 	  //Click Event added to Button Element
+	 // 	  button.addEventListener('click', function () {
+	 // 	  	button.parentNode.removeChild(button);
+	 // 	  	span.classList.add('visible');
+	 // 	  });
+	 // };
 
-	 for (var i = 0; i < elements.length; i++) {
-	 	  createSpoilerButton(elements[i]);
-	 }
+	 // for (var i = 0; i < elements.length; i++) {
+	 // 	  createSpoilerButton(elements[i]);
+	 // }
 
+ //+++++++++++++++++++++++++++++++++++++++++++++++++++
 
+	  
+	  var body = document.querySelector('body.top-content');
+
+	  var ias = function () {
+	    var a = document.createElement('script');
+	    a.type = "text/javascript";
+	    a.async = true;
+	    a.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
+
+	    console.log(a);
+
+	    var b = document.getElementsByTagName("script")[0];
+
+	    console.log(b);
+
+	    b.parentNode.insertBefore(a, b);
+
+	    console.log(b.parentNode);
+	  };
+
+	  body.append(ias());
 
 });
