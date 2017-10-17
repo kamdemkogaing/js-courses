3ec6ade37cb91a26a011981d2a8c71afcf27b18d
kamdem <kamdem@kamdems-MBP.fritz.box>
2017-10-18 00:26:33 +0200

    some commit to 18_10_17

--- js/tp1.js
+++ js/tp1.js
@@ -1,72 +1,74 @@
 $(document).ready(function () {
-	/**
-		LORSQUE JE clique sur le boutton dans .spolier
-		ALORS
-		   j'ajoute la classe .visible á l'element suivant
-	**/
+  /**
+  LORSQUE JE clique sur le boutton dans .spolier
+  ALORS
+   j'ajoute la classe .visible á l'element suivant
+  **/
 
-	// var button = document.querySelector('.spoiler button');
+   // var button = document.querySelector('.spoiler button');
 
-	// button.addEventListener('click', function () {
-	// 	this.nextElementSibling.classList.add('visible');
-	// 	this.parentNode.removeChild(this);
-	// });
+   // button.addEventListener('click', function () {
+   //  this.nextElementSibling.classList.add('visible');
+   //  this.parentNode.removeChild(this);
+   // });
 
-	//+++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++
 
-  //  //Select all element with Class .spoiler in to an array
-	 // var elements = document.querySelectorAll('.spoiler');
+    //Select all element with Class .spoiler in to an array
+    var elements = document.querySelectorAll('.spoiler');
+    
 
-  //  //element = some className like  ".spoiler"
-	 // var createSpoilerButton = function (element) {
+    //element = some className like  ".spoiler"
+    var createSpoilerButton = function (element) {
 
-  //    //Button Element created
-	 // 	 var button = document.createElement('button');
-	 // 	 button.innerHTML = 'Afficher le spoiler';
+      //Button Element created
+      var button = document.createElement('button');
+      button.innerHTML = 'Afficher le spoiler';
 
-  //    //Span Element created
-	 // 	 var span = document.createElement('span');
-	 // 	 span.className = 'spoiler-content';
-	 // 	 span.innerHTML = element.innerHTML;
+      //Span Element created
+      var span = document.createElement('span');
+      span.className = 'spoiler-content';
+      span.innerHTML = element.innerHTML;
 
-  //     //Elements added to DOM
-  //     element.innerHTML = '';
-	 // 	  element.appendChild(button);
-	 // 	  element.appendChild(span);
+      //Elements added to DOM
+      element.innerHTML = '';
+      element.appendChild(button);
+      element.appendChild(span);
 
-	 // 	  //Click Event added to Button Element
-	 // 	  button.addEventListener('click', function () {
-	 // 	  	button.parentNode.removeChild(button);
-	 // 	  	span.classList.add('visible');
-	 // 	  });
-	 // };
+       //Click Event added to Button Element
+       button.addEventListener('click', function () {
+         button.parentNode.removeChild(button);
+         span.classList.add('visible');
+       });
+    };
 
-	 // for (var i = 0; i < elements.length; i++) {
-	 // 	  createSpoilerButton(elements[i]);
-	 // }
+    for (var i = 0; i < elements.length; i++) {
+     createSpoilerButton(elements[i]);
+    }
 
  //+++++++++++++++++++++++++++++++++++++++++++++++++++
 
 	  
-	  var body = document.querySelector('body.top-content');
+   var body = document.querySelector('body.top-content');
 
-	  var ias = function () {
-	    var a = document.createElement('script');
-	    a.type = "text/javascript";
-	    a.async = true;
-	    a.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
+   var ias = function () {
+     var a = document.createElement('script');
+     a.type = "text/javascript";
+     a.async = true;
+     a.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
 
-	    console.log(a);
+     console.log(a);
 
-	    var b = document.getElementsByTagName("script")[0];
+     var b = document.getElementsByTagName("script")[0];
 
-	    console.log(b);
+     console.log(b);
 
-	    b.parentNode.insertBefore(a, b);
+     b.parentNode.insertBefore(a, b);
 
-	    console.log(b.parentNode);
-	  };
+     console.log(b.parentNode);
+  
+   };
 
-	  body.append(ias());
+   body.append(ias());
 
 });
