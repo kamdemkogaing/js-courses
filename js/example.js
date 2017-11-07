4a7a7f035204fbf2999a3c02793d4d60f1706ab6
kamdem <kamdem@kamdems-MBP.fritz.box>
2017-11-07 22:45:33 +0100

    Commit-Nachricht

--- js/example.js
+++ js/example.js
@@ -1,16 +1,12 @@
 $(document).ready(function () {
 	(function () {
-	 var d = document,
-	      g = d.createElement('script'),
-	      s = d.getElementsByTagName('script')[0];
+	 var  g = document.createElement('script'),
+	      s = document.getElementsByTagName('script')[0];
 	      g.type = 'text/javascript';
 	      g.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
 	      s.parentNode.insertBefore(g, s);
 
-	      console.log(d);
 
-	      var axel = Math.random() + "";
-        var a = axel * 10000000000000;
-        d.write('the value from a is:'+a);
+	      document.write('<html><h2>PATRICK KAMDEM</h2></html>');
 	})();	
 });
