12e0a341b39235c96372eccf1c793adcb560713b
kamdemkogaing <31521726+kamdemkogaing@users.noreply.github.com>
2018-02-22 15:53:33 +0100

    oop js closures

--- js/oo_js_scopes.js
+++ js/oo_js_scopes.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
- //Scopes and Closures
+ /* Scopes */
  /* Example 1*/
 
- //what Variables are available from within the body of the newSaga function?
- //Answer: hero is in global scope, so it can be accessed inside newSaga, newSaga itself is also in global scope, foil is in the scope of the function body, so it can be accessed, too.
+  //what Variables are available from within the body of the newSaga function?
+  //Answer: hero is in global scope, so it can be accessed inside newSaga, newSaga itself is also in global scope, foil is in the scope of the function body, so it can be accessed, too.
 
 	var hero = aHero();
 
@@ -14,9 +14,10 @@ $(document).ready(function () {
 		console.log(foil);
 	};
 
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	/* Example 2*/
-	//JavaScript allows you to assign to variables you've
+	//JavaScript allows you to assign to variables you've never even declared before. (Bad pratice)
   var hero = aHero();
 
   var newSaga = function () {
@@ -25,4 +26,34 @@ $(document).ready(function () {
 
   console.log(foil);
 
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /* Example 3*/
+  // looping constructs don't create new scopes
+  var hero = aHero();
+
+  if (checkSomething()) {
+  	var foil = aFoil();
+  }
+
+  console.log(foil);
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  /* Example 4*/
+  // Calling the functions
+  var hero = "Gal";
+
+  var newSaga = function () {
+  	var foil = "Cow";
+  	var saga = function () {
+  		var deed = "Eyes";
+  		console.log(hero+deed+foil);
+  	};
+  	saga();
+  };
+  newSaga();
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
 });
