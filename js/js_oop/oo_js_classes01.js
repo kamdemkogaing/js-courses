84f2f276c6ac0d9feff88f14944b3c271b8d6f9c
kamdemkogaing <31521726+kamdemkogaing@users.noreply.github.com>
2018-04-24 13:58:48 +0200

    js pattern 24_04_18

--- js/js_oop/oo_js_classes01.js
+++ js/js_oop/oo_js_classes01.js
@@ -18,15 +18,33 @@ $(document).ready(function() {
 
 	var amy = new Car(1);
 	amy.move();
-	console.log(amy);
+	// console.log(amy);
 
 	var ben = new Car(9);
 	ben.move();
-	console.log(ben);
+	// console.log(ben);
+
 
 	/*Learn how to utilize the this keyword in yout superclasses, set
 	  properties on your classes using .call(), and delegate prototypes to subclasses.*/
+ 
+  var Car1 = function(loc1) {
+  	var obj = {loc1: loc1};
+  	obj.move1 = function() {
+  			obj.loc1++;
+  		};
+  	return obj;
+  };
 
-	 
+  var amy1 = Car1(1);
+  amy1.move1();
+  console.log(amy1);
 
+  var ben1 = Car1(9);
+  ben1.move1();
+  console.log(ben1);
+
+  var cal = Car1(2)
+  cal.move1();
+  console.log(cal);
 });
