3239e3cd6ae8bdd5c1ba6827a6bb63d9693a85b5
kamdemkogaing <31521726+kamdemkogaing@users.noreply.github.com>
2018-07-09 10:53:45 +0200

    some updates

--- js/js_oop/oo_js_subclasses.js
+++ js/js_oop/oo_js_subclasses.js
@@ -39,4 +39,25 @@ $(document).ready(function() {
 		};
 	})();
 
+	//Using call() function
+	(function() {
+		var Car = function(loc) {
+			this.loc = loc;
+		};
+		Car.prototype.move = function() {
+			this.loc++;
+		};
+
+		var Van = function(loc){
+			Car.call(this, loc);
+		};
+
+		var zed = new Car(3);
+		zed.move();
+
+		var amy = new Van(9);
+		amy.move();
+		amy.grab();
+	})();
+
 });
